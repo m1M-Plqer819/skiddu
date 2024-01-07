bc8fde2180b7d76e3e058f84a88b6977fd2f97a1
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-07 08:40:27 +0700

    Update sgb.lua

--- banin/sgb.lua
+++ banin/sgb.lua
@@ -1,6 +1,6 @@
 repeat wait()
 until game:IsLoaded()
-local TableChat = {"what the fuck roblox trash game","say goodbye!","script by m1m","dick","child porn!","fuck you roblox","EZ EZ EZ EZ EZ EZ EZ EZ EZ EZ","have sex to night"}
+local TableChat = {"Prick","say goodbye!","script by m1m","Fucker","sod","shit","EZ EZ EZ roblox trash game fuck dick tits fart...","Bastard","Cunt","bitch","wanker","bugger","asshole,"son of a bitch","motherfucker","cocksucker","dickhead","sex","dick","pussy","arsehole","bollocks","fuck","piss","whore","tits"}
 spawn(function()
     while wait() do 
         pcall(function()
