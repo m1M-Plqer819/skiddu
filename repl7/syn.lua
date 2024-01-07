c35d527fc7eba64602b403a78240703066e926a2
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-07 14:30:07 +0700

    Create syn.lua

---
+++ repl7/syn.lua
@@ -0,0 +1,151 @@
+_G.Team = "Pirates"
+
+_G.FpsBoost = true
+
+_G.Webhook = "https://discord.com/api/webhooks/1187340197927604254/OZMxPvP2-EQxMGnLvs6RyegvfIiKXFK5DWUXPZ_f_IIkGLOmUqdTJU9c2Cd_qnP3qNLS"
+
+
+getgenv().Chatkill = {
+
+    "@[c/f by (. M-1-M .)", "*#^%&$?<>|:}{+_~!]",
+
+}
+
+getgenv().Region_Hop = {
+
+    ["Enable"] = true, -- true/false
+
+    ["Region"] = "United States"
+
+    --[[Singapore, United States, Germany, Japan, France, Australia, etc
+
+        - Singapore is the best choice for Asia
+
+    ]]
+
+}
+
+_G.Setting = {
+
+    ["Start Hunting"] = true, -- Bro wtf don't turn it off
+
+    ["Ken Haki"] = true,
+
+    ["WhiteScreen"] = false,
+
+    ["FruitStuff"] = false, -- Auto Buy and Store Fruits
+
+    ["Run"] = 4835, -- As the name, you'll run when the health below ...
+
+    ["MaxHealth"] = 8649,
+
+    ["Time&Bounty Counter"] = true,
+
+    ["Click Delay"] = 0.2,
+
+    ["SkipPlayerWhenBeingLowHealth"] = false,
+
+    ["Check"] = {
+
+        ["V4 Players"] = true,
+
+        ["Portal Users"] = true,
+
+        ["Buddha Users"] = false
+
+    },
+
+    ["Theme"] = { -- You can get RGB colors from "Color Picker" on google
+
+        ["Default"] = "YaeMiko", -- Use this if you don't know how to customize the theme, get themes on: https://github.com/clgtmm/ThemeBuilds
+
+        ["Custom Theme"] = {
+
+            ["Enable"] = false, -- Set this "false" if not knowing how to customize the theme
+
+            ["Color"] = {
+
+                ["Title Color"] = Color3.fromRGB(168, 129, 50),
+
+                ["Title-Back Color"] = Color3.fromRGB(168, 129, 50),
+
+                ["Button Color"] = Color3.fromRGB(168, 129, 50),
+
+                ["Stats Color"] = {
+
+                    ["Text Color"] = Color3.fromRGB(50, 168, 82),
+
+                    ["Text Stroke"] = Color3.fromRGB(133, 50, 168)
+
+                }
+
+            }
+
+        }
+
+    },
+
+    ["Melee"] = {
+
+        ["Time"] = 2.5,
+
+        ["Enable"] = true,
+
+        ["Z"] = {["Enable"] = true, ["HoldTime"] = 1.75},
+
+        ["X"] = {["Enable"] = true, ["HoldTime"] = 0.5},
+
+        ["C"] = {["Enable"] = true, ["HoldTime"] = 0.5}
+
+    },
+
+    ["Fruit"] = {
+
+        ["Time"] = 3,
+
+        ["Enable"] = true,
+
+        ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.5},
+
+        ["X"] = {["Enable"] = true, ["HoldTime"] = 0.5},
+
+        ["C"] = {["Enable"] = true, ["HoldTime"] = 1.75},
+
+        ["V"] = {["Enable"] = true, ["HoldTime"] = 1},
+
+        ["F"] = {["Enable"] = false, ["HoldTime"] = 0}
+
+    },
+
+    ["Sword"] = {
+
+        ["Time"] = 1.5,
+
+        ["Enable"] = true,
+
+        ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.5},
+
+        ["X"] = {["Enable"] = true, ["HoldTime"] = 0.5},
+
+    },
+
+    ["Gun"] = {
+
+        ["Time"] = 2,
+
+        ["Enable"] = true,
+
+        ["GunMode"] = false,
+
+        ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.5},
+
+        ["X"] = {["Enable"] = true, ["HoldTime"] = 0.5},
+
+    }
+
+}
+
+
+repeat wait() until game:IsLoaded()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/clgtmm/AutoBounty/main/main.lua"))()
