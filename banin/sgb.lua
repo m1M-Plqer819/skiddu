ddd2748d3819c726ebb1533cecdbc67497f6842c
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-07 08:53:31 +0700

    Update sgb.lua

--- banin/sgb.lua
+++ banin/sgb.lua
@@ -1,6 +1,6 @@
 repeat wait()
 until game:IsLoaded()
-local TableChat = {"Prick","goodbye!","script by m1m","Fucker","sod","Bastard","Cunt","bitch","wanker","bugger","asshole,"son of a bitch","motherfucker","cocksucker","dickhead","sex","dick","pussy","arsehole","bollocks","fuck","piss","whore","tits"}
+local TableChat = {"Prick","goodbye!","script by m1m","Fucker","sod","Bastard","Cunt","bitch","wanker","bugger","asshole,"motherfucker","cocksucker","dickhead","sex","dick","pussy","arsehole","bollocks","fuck","piss","whore","tits"}
 spawn(function()
     while wait() do 
         pcall(function()
