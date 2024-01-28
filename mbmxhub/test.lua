ba846b023f9dec9d484ed2e71660ba0f5c06c371
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-28 13:19:53 +0700

    Update test.lua

--- mbmxhub/test.lua
+++ mbmxhub/test.lua
@@ -1,3 +1,40 @@
+local AutoJoinTeam = "Marines" -- "Marines" or "Pirates"
+
+local function setPlayerTeam(teamName)
+    local replicatedStorage = game:GetService("ReplicatedStorage")
+    local player = game:GetService("Players").LocalPlayer
+    local playerGui = player.PlayerGui
+    local backpack = player.Backpack
+    local character = player.Character
+
+    replicatedStorage.Remotes["CommF_"]:InvokeServer("SetTeam", teamName)
+
+    local elementsToShow = {
+        "RaceEnergy", "Compass", "Energy", "AlliesButton", "Code",
+        "CrewButton", "HomeButton", "Mute", "Settings", "MenuButton",
+        "Beli", "Fragments", "Level", "HP"
+    }
+
+    local chooseTeamFrame = playerGui.Main:FindFirstChild("ChooseTeam")
+    if chooseTeamFrame then
+        chooseTeamFrame:Destroy()
+    end
+
+    for _, elementName in pairs(elementsToShow) do
+        local element = playerGui.Main[elementName]
+        if element then
+            element.Visible = (elementName == "RaceEnergy") and (backpack:FindFirstChild("Awakening") or character:FindFirstChild("Awakening")) or true
+        end
+    end
+
+    local camera = game:GetService("Workspace").CurrentCamera
+    camera.CameraType = Enum.CameraType.Custom
+    camera.CameraSubject = character.Humanoid
+    camera.CFrame = character.HumanoidRootPart.CFrame
+end
+
+setPlayerTeam(AutoJoinTeam)
+
 spawn(function()
 while wait() do
 if _G.ChestBypass then
@@ -30,24 +67,6 @@ if _G.ChestBypass then
     game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(ohString1, ohString2)
  end
 end
-end)
-
-noclip = true
-game:GetService('RunService').Stepped:Connect(function()
-	if noclip then
-		game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-		
-	end
-end)
-
-local p = game.Players.LocalPlayer
-local mo = p:GetMouse()
-
-mo.KeyDown:Connect(function(k)
-	if k == 'e' then
-		noclip = not noclip
-		p.Character.Humanoid:ChangeState(11)
-	end
 end)
 
 spawn(function()
