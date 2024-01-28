21eb174102c23d3d37056fbbcf05461d16bd3acf
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-28 20:30:21 +0700

    Update test.lua

--- mbmxhub/test.lua
+++ mbmxhub/test.lua
@@ -1,43 +1,156 @@
-spawn(function()
-while wait() do
 if _G.ChestBypass then
-pcall(function()
-for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
-  if string.find(v.Name, "Chest") then
-      print(v.Name)
-      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
-      wait(.15)
-  end
-end
-game.Players.LocalPlayer.Character.Head:Destroy()
-for _,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-if string.find(v.Name, "Chest") and v:IsA("TouchTransmitter") then
-firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0) --0 is touch
-wait()
-firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1) -- 1 is untouch
-end
-end
-end)
-end
-end
-end)
-
-spawn(function()
-while task.wait() do
-if _G.ChestBypass then
-    local ohString1 = "SetTeam"
-    local ohString2 = "Pirates"
-    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(ohString1, ohString2)
- end
-end
-end)
-
-spawn(function()
-    while wait() do
-    for i,v in pairs(game.Players:GetPlayers()) do
-        if v.Name == "red_game43" or v.Name == "rip_indra" or v.Name == "Axiore" or v.Name == "Polkster" or v.Name == "wenlocktoad" or v.Name == "Daigrock" or v.Name == "toilamvidamme" or v.Name == "oofficialnoobie" or v.Name == "Uzoth" or v.Name == "Azarth" or v.Name == "arlthmetic" or v.Name == "Death_King" or v.Name == "Lunoven" or v.Name == "TheGreateAced" or v.Name == "rip_fud" or v.Name == "drip_mama" or v.Name == "layandikit12" or v.Name == "Hingoi" then
-            Hop()
+    repeat
+        task.wait(4)
+    until game:IsLoaded()
+    local a = game.PlaceId
+    local b = {}
+    local c = ""
+    local d = os.date("!*t").hour
+    local e = false
+    local e =
+        pcall(
+        function()
+            b = game:GetService("HttpService"):JSONDecode(readfile("NotSameServers.json"))
+        end
+    )
+    if not e then
+        table.insert(b, d)
+        writefile("NotSameServers.json", game:GetService("HttpService"):JSONEncode(b))
+    end
+    function TPReturner()
+        local e
+        if c == "" then
+            e =
+                game.HttpService:JSONDecode(
+                game:HttpGet("https://games.roblox.com/v1/games/" .. a .. "/servers/Public?sortOrder=Asc&limit=100")
+            )
+        else
+            e =
+                game.HttpService:JSONDecode(
+                game:HttpGet(
+                    "https://games.roblox.com/v1/games/" .. a .. "/servers/Public?sortOrder=Asc&limit=100&cursor=" .. c
+                )
+            )
+        end
+        local f = ""
+        if e.nextPageCursor and e.nextPageCursor ~= "null" and e.nextPageCursor ~= nil then
+            c = e.nextPageCursor
+        end
+        local c = 0
+        for e, e in pairs(e.data) do
+            local g = true
+            f = tostring(e.id)
+            if tonumber(e.maxPlayers) > tonumber(e.playing) then
+                for a, a in pairs(b) do
+                    if c ~= 0 then
+                        if f == tostring(a) then
+                            g = false
+                        end
+                    else
+                        if tonumber(d) ~= tonumber(a) then
+                            local a =
+                                pcall(
+                                function()
+                                    delfile("NotSameServers.json")
+                                    b = {}
+                                    table.insert(b, d)
+                                end
+                            )
+                        end
+                    end
+                    c = c + 1
+                end
+                if g == true then
+                    table.insert(b, f)
+                    wait()
+                    pcall(
+                        function()
+                            writefile("NotSameServers.json", game:GetService("HttpService"):JSONEncode(b))
+                            wait()
+                            game:GetService("TeleportService"):TeleportToPlaceInstance(a, f, game.Players.LocalPlayer)
+                        end
+                    )
+                    wait(4)
+                end
             end
         end
     end
-end)
+    function Teleport()
+        while wait() do
+            pcall(
+                function()
+                    TPReturner()
+                    if c ~= "" then
+                        TPReturner()
+                    end
+                end
+            )
+        end
+    end
+    local a = 0.5
+    task.spawn(
+        function()
+            while task.wait(a) do
+                pcall(
+                    function()
+                        for b, b in pairs(game.CoreGui:GetDescendants()) do
+                            pcall(
+                                function()
+                                    if string.find(b.Name, "ErrorMessage") then
+                                        if string.find(b.Text, "Security kick") then
+                                            a = 1e9
+                                            Teleport()
+                                        end
+                                    end
+                                end
+                            )
+                        end
+                    end
+                )
+            end
+        end
+    )
+    AllowRunService = true
+    task.spawn(
+        function()
+            while true and task.wait(.5) do
+                if AllowRunService == true then
+                    local a = "SetTeam"
+                    local b = "Marines"
+                    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(a, b)
+                end
+            end
+        end
+    )
+    task.spawn(
+        function()
+            while true and task.wait() do
+                if AllowRunService == true then
+                    local a = game.Players.LocalPlayer:FindFirstChild("Character")
+                    if not game.Players.LocalPlayer.Character then
+                    else
+                        local a = game.Players.LocalPlayer.Character:FindFirstChild("CrewBBG", true)
+                        if a then
+                            a:Destroy()
+                        end
+                        local a = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+                        if a then
+                            local a =
+                                game.Workspace:FindFirstChild("Chest4") or game.Workspace:FindFirstChild("Chest3") or
+                                game.Workspace:FindFirstChild("Chest2") or
+                                game.Workspace:FindFirstChild("Chest1") or
+                                game.Workspace:FindFirstChild("Chest")
+                            if a then
+                                game.Players.LocalPlayer.Character:PivotTo(a:GetPivot())
+                                firesignal(a.Touched, game.Players.LocalPlayer.Character.HumanoidRootPart)
+                            else
+                                Teleport()
+                                break
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    )
+end
