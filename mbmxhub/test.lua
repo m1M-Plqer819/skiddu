3cf9ac5d6d7332d29c2eedad5be35f4172637d5c
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-28 12:48:59 +0700

    Update test.lua

--- mbmxhub/test.lua
+++ mbmxhub/test.lua
@@ -1,1 +1,33 @@
+spawn(function()
+while wait() do
+if _G.ChestBypass then
+pcall(function()
+for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
+  if string.find(v.Name, "Chest") then
+      print(v.Name)
+      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+      wait(.15)
+  end
+end
+game.Players.LocalPlayer.Character.Head:Destroy()
+for _,v in pairs(game:GetService("Workspace"):GetDescendants()) do
+if string.find(v.Name, "Chest") and v:IsA("TouchTransmitter") then
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0) --0 is touch
+wait()
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1) -- 1 is untouch
+end
+end
+end)
+end
+end
+end)
 
+spawn(function()
+while task.wait() do
+if _G.ChestBypass then
+    local ohString1 = "SetTeam"
+    local ohString2 = "Pirates"
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(ohString1, ohString2)
+ end
+end
+end)
