620c5a4df624b02bc43d2b57a56d513fb52ea81e
M.1.M-Plqer819(glitched) <143993974+m1M-Plqer819@users.noreply.github.com>
2024-01-28 20:03:14 +0700

    Update test.lua

--- mbmxhub/test.lua
+++ mbmxhub/test.lua
@@ -1,1 +1,43 @@
+spawn(function()
+while wait() do
+if _G.ChestBypass then
+pcall(function()
+for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
+  if string.find(v.Name, "Chest") then
+      print(v.Name)
+      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+      wait(.15)
+  end
+end
+game.Players.LocalPlayer.Character.Head:Destroy()
+for _,v in pairs(game:GetService("Workspace"):GetDescendants()) do
+if string.find(v.Name, "Chest") and v:IsA("TouchTransmitter") then
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0) --0 is touch
+wait()
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1) -- 1 is untouch
+end
+end
+end)
+end
+end
+end)
 
+spawn(function()
+while task.wait() do
+if _G.ChestBypass then
+    local ohString1 = "SetTeam"
+    local ohString2 = "Pirates"
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(ohString1, ohString2)
+ end
+end
+end)
+
+spawn(function()
+    while wait() do
+    for i,v in pairs(game.Players:GetPlayers()) do
+        if v.Name == "red_game43" or v.Name == "rip_indra" or v.Name == "Axiore" or v.Name == "Polkster" or v.Name == "wenlocktoad" or v.Name == "Daigrock" or v.Name == "toilamvidamme" or v.Name == "oofficialnoobie" or v.Name == "Uzoth" or v.Name == "Azarth" or v.Name == "arlthmetic" or v.Name == "Death_King" or v.Name == "Lunoven" or v.Name == "TheGreateAced" or v.Name == "rip_fud" or v.Name == "drip_mama" or v.Name == "layandikit12" or v.Name == "Hingoi" then
+            Hop()
+            end
+        end
+    end
+end)
